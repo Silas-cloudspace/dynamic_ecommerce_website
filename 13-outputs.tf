a93c087d37da5406676bc953c745341eae2a2271
Silas Teixeira <166008901+Silas-cloudspace@users.noreply.github.com>
2024-08-01 18:20:20 +0100

    Update 13-outputs.tf

--- 13-outputs.tf
+++ 13-outputs.tf
@@ -11,7 +11,7 @@ value = join ("",["https://", "www", ".", "your domain name"])
 }
 
 # "www" is the name we picked in the "aws_reoute53_record"
-# "silas-teixeira" is the "domain_name" we picked when creating the "acm_certificate" 
+# "your domain name" is the "domain_name" we picked when creating the "acm_certificate" 
 # "https://" we add it to complete the full url
 # If we now do "terrafrom apply", we can check our website link
 # If we (ctrl+click) in it, it will open our website
